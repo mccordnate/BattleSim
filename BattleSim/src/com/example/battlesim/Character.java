4d5eda83bc6b658417657fcd1d936bd39fdf0d54
Ansh <ansh.ratnam@gmail.com>
2013-11-06 17:39:18 -0600

    Created Character Class

---
+++ BattleSim/src/com/example/battlesim/Character.java
@@ -0,0 +1,13 @@
+package com.example.battlesim;
+
+public abstract class Character {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
